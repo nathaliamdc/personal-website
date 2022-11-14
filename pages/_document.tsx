3c7ff4aa09d71a95cd4fa9f9acbebcae3a55ac9a
Nathalia <mdc.nathalia@gmail.com>
2022-11-14 12:53:54 +0200

    Add public amplitude api key

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap"
